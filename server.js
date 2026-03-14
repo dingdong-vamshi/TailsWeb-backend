68798262f1875080b468f4cf5f343ba6d822e1c7
Vamshi Krishna Pendyala <vamshikrishnapendyala19@gmail.com>
2026-03-14 11:24:02 +0530

    assignment api

--- server.js
+++ server.js
@@ -70,6 +70,71 @@ app.get('/initdb', async (req, res) => {
     res.send('done init');
 });
 
+app.post('/assignment/create', checkTeacher, async (req, res) => {
+    let d1 = req.body;
+    let newA = new Assignment({
+        title: d1.title,
+        description: d1.description,
+        dueDate: d1.dueDate,
+        status: 'Draft' 
+    });
+    await newA.save();
+    res.json(newA);
+});
+
+app.get('/assignment', async (req, res) => {
+    let t = req.headers.authorization;
+    if(!t) return res.status(401).json({msg: 'no token'});
+    try {
+        let dec = jwt.verify(t.split(" ")[1], SECRET_KEY);
+        if(dec.role === 'teacher') {
+            let a = await Assignment.find({});
+            res.json(a);
+        } else {
+            let a = await Assignment.find({status: 'Published'});
+            res.json(a);
+        }
+    } catch(e) {
+        res.status(400).json({msg: 'error'});
+    }
+});
+
+app.put('/assignment/update', checkTeacher, async (req, res) => {
+    let id_ = req.body.id;
+    let obj1 = await Assignment.findById(id_);
+    if(obj1.status === 'Draft') {
+        obj1.title = req.body.title || obj1.title;
+        obj1.description = req.body.description || obj1.description;
+        obj1.dueDate = req.body.dueDate || obj1.dueDate;
+        await obj1.save();
+        res.json(obj1);
+    } else {
+        res.status(400).json({msg: 'Cant update published'});
+    }
+});
+
+app.put('/assignment/publish', checkTeacher, async (req, res) => {
+    let aid = req.body.id;
+    let a = await Assignment.findById(aid);
+    if(a) {
+        a.status = 'Published';
+        await a.save();
+        res.json(a);
+    } else {
+        res.status(404).send('Not found');
+    }
+});
+
+app.delete('/assignment/delete/:id', checkTeacher, async (req, res) => {
+    let a = await Assignment.findById(req.params.id);
+    if(a && a.status === 'Draft') {
+        await Assignment.findByIdAndDelete(req.params.id);
+        res.json({msg: 'deleted'});
+    } else {
+        res.status(400).json({msg: 'cannot delete'});
+    }
+});
+
 app.listen(5000, () => {
     console.log('server on 5000 !!');
 });
