6fcd56042418ffa91ae1320d10e88679336faf14
Vamshi Krishna Pendyala <vamshikrishnapendyala19@gmail.com>
2026-03-14 11:23:38 +0530

    jwt login

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// conn data
 mongoose.connect('mongodb://127.0.0.1:27017/assignment_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -36,6 +37,38 @@ const SubSchema = new mongoose.Schema({
 const Sub = mongoose.model('Submission', SubSchema);
 
 const SECRET_KEY = "my_super_secret_key_123_temp";
+
+const checkTeacher = (req, res, next) => {
+    let t = req.headers.authorization;
+    if(!t) return res.status(401).json({msg: 'no token'});
+    try {
+        let decoded = jwt.verify(t.split(" ")[1], SECRET_KEY);
+        if(decoded.role !== 'teacher') return res.status(403).json({msg: 'not teacher'});
+        req.uInfo = decoded;
+        next();
+    } catch(e) {
+        res.status(400).json({msg: 'bad token'});
+    }
+}
+
+app.post('/login', async (req, res) => {
+    let e = req.body.email;
+    let p = req.body.password;
+    
+    let u = await User.findOne({email: e, password: p});
+    if(u) {
+        let token123 = jwt.sign({id: u._id, role: u.role}, SECRET_KEY, {expiresIn: '2h'});
+        res.json({token: token123, role: u.role});
+    } else {
+        res.status(400).json({err: 'wrong creds'});
+    }
+});
+
+app.get('/initdb', async (req, res) => {
+    await User.create({email: 'teacher@test.com', password: '123', role: 'teacher'});
+    await User.create({email: 'student@test.com', password: '123', role: 'student'});
+    res.send('done init');
+});
 
 app.listen(5000, () => {
     console.log('server on 5000 !!');
